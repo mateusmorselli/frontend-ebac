ad611a6f8ea122859c55517723a4ccf5fd8d570e
Mateus Morselli <163318844+mateusmorselli@users.noreply.github.com>
2025-03-25 18:40:02 -0300

    Delete modulo-31 directory

--- modulo-31/PRACTICE/src/components/Adicionar/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Circulo } from './styles'
-
-const BotaoAdicionar = () => {
-  return <Circulo to="/novo">+</Circulo>
-}
-
-export default BotaoAdicionar
--- modulo-31/PRACTICE/src/utils/enums/TiposDeContatos.ts
+++
@@ -1,7 +0,0 @@
-export enum TipoDeContato {
-  AMIGOS = 'amigos',
-  FAMILIA = 'família',
-  TRABALHO = 'trabalho',
-  OUTROS = 'outros',
-  TODOS = 'todos'
-}
